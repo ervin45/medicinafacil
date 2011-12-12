8df25a41e9c3b11c84fbe4243d3d40edcaad38b4
eugenioamancebo <eugenioamancebo@gmail.com>
2011-12-12 22:42:58 +0000

    commit

---
+++ SistemaMedico/src/com/sistemamedico/entidades/Paciente.java
@@ -0,0 +1,41 @@
+package com.sistemamedico.entidades;
+
+public class Paciente {
+
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String celular;
+	private String cedula;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+}
